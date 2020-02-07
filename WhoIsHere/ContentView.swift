e955af3c480355a53ba70a1856638ec4d4116ef2
Valentin Vincent <valentinhov95@gmail.com>
2020-02-07 09:53:17 +0100

    Initial Commit

---
+++ WhoIsHere/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WhoIsHere
+//
+//  Created by Vincent Valentin on 07/02/2020.
+//  Copyright © 2020 Vincent Valentin. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
